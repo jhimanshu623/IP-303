169a89e63dcbb84c609856ff4728e297deb56cc7
jhimanshu623 <jhimanshu623@gmail.com>
2021-10-14 00:49:09 +0530

    lecture 10 p2 update

--- lecture_10/p2.java
+++ lecture_10/p2.java
@@ -15,6 +15,20 @@ public class p2 {
         }
         return ans;
     }
+
+    // for zeros or ones
+    public static int solve(int arr[]){
+        int c_count=1,o_count=1;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]==arr[i-1]){
+                c_count++;
+            }else{
+                c_count=1;
+            }
+            o_count=Math.max(o_count,c_count);
+        }
+        return o_count;
+    }
     public static void main(String[] args) {
         int arr[] ={1,1,0,0,1,1,1,1,0,0,0,1,0,1,1,1,1,1,1};
         System.out.println(max_1(arr));
